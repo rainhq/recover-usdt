9244934f2079ea54487ee77ac7814a76141b27ab
Joseph Dallago <jd.dallago@gmail.com>
2020-11-24 10:40:05 +0300

    Modified recover.js to not require a backup key to function.

--- recover.js
+++ recover.js
@@ -4,17 +4,17 @@
  * TODO: fill out the below fields with your specific information for this recovery
  */
 
-const address = ''; // the bitgo address where the usdt is stuck
-const walletPasscode = ''; // the passcode for the bitgo wallet that contains the above address
-const fundingTxHex = ''; // the transaction hex for any utxo owned by the above address - this will fund the recovery transaction
-const destAddr = ''; // the destination address where you'd like the tether sent to
+const address = process.env.USDT_RECOVERY_ADDRESS; // the bitgo address where the usdt is stuck
+const walletPasscode = process.env.USDT_RECOVERY_WALLET_PASSCODE; // the passcode for the bitgo wallet that contains the above address
+const fundingTxHex = process.env.USDT_RECOVERY_FUNDING_TX_HEX; // the transaction hex for any utxo owned by the above address - this will fund the recovery transaction
+const destAddr = process.env.USDT_RECOVERY_DEST_ADDR; // the destination address where you'd like the tether sent to
 const changeAddr = address; // the address where change from the transaction will be sent. recommended to leave this as is (changeAddr = address)
-const tetherAmount = 1e8; // the amount of tether to send in the recovery transaction (1e8 = 1 usdt)
-const ep1 = {}; // the value of Box A of your wallet keycard - this is your encrypted user key
-const ep2 = {}; // the value of Box B of your wallet keycard - this is your encrypted backup key
-const bitgoPublicKey = ''; // the value of Box C of your wallet keycard - this is the bitgo public key for your wallet
-const addressIndex = -1; // this is the 'index' of the address being used. find this using BitGoJS and the 'wallet.getAddress({address})' function (if unsure, contact support@bitgo.com)
-const addressChain = 10; // this is the 'chain' of the address being used. find this using BitGoJS and the 'wallet.getAddress({address})' function (if unsure, contact support@bitgo.com)
+const tetherAmount = parseInt(process.env.USDT_RECOVERY_TETHER_AMOUNT, 10); // the amount of tether to send in the recovery transaction (1e8 = 1 usdt)
+const ep1 = JSON.parse(process.env.USDT_RECOVERY_EP1); // the value of Box A of your wallet keycard - this is your encrypted user key
+const ep2 = JSON.parse(process.env.USDT_RECOVERY_EP2); // the value of Box B of your wallet keycard - this is your encrypted backup key
+const bitgoPublicKey = process.env.USDT_RECOVERY_BITGO_PUBLIC_KEY; // the value of Box C of your wallet keycard - this is the bitgo public key for your wallet
+const addressIndex = parseInt(process.env.USDT_RECOVERY_ADDRESS_INDEX, 10); // this is the 'index' of the address being used. find this using BitGoJS and the 'wallet.getAddress({address})' function (if unsure, contact support@bitgo.com)
+const addressChain = parseInt(process.env.USDT_RECOVERY_ADDRESS_CHAIN, 10); // this is the 'chain' of the address being used. find this using BitGoJS and the 'wallet.getAddress({address})' function (if unsure, contact support@bitgo.com)
 const mainnet = true; // leave as true for mainnet. Set to false for testnet
 /**
  * End of custom fields
@@ -48,7 +48,7 @@ const signTx = function(pubkeys, prvs, path, scripts) {
     const fundingUTXO = getFundingUTXO(inTx);
     const txInAmt = fundingUTXO.value;
     const dust = Math.round(.0000059 * toBig);
-    const miningFee = Math.round(.000122 * toBig);
+    const miningFee = Math.round(.0000122 * toBig);
     const changeAmt = Math.round(txInAmt - dust - miningFee);
     const txb = new bitcoin.TransactionBuilder(network);
 
@@ -65,16 +65,12 @@ const signTx = function(pubkeys, prvs, path, scripts) {
 
     let keyPair1 = bip32.fromBase58(prvs[0]);
     keyPair1.network = network;
-    let keyPair2 = bip32.fromBase58(prvs[1]);
-    keyPair2.network = network;
 
     keyPair1 = keyPair1.derivePath(path);
-    keyPair2 = keyPair2.derivePath(path);
 
     txb.sign(0, keyPair1, scripts.p2sh.redeem.output, null, txInAmt, scripts.p2wsh.redeem.output);
-    txb.sign(0, keyPair2, scripts.p2sh.redeem.output, null, txInAmt, scripts.p2wsh.redeem.output);
 
-    const finalTx = txb.build();
+    const finalTx = txb.buildIncomplete();
 
     console.log(`Completed signing recovery transaction. Broadcast the following tx hex at https://${mainnet ? 'www' : 'testnet'}.smartbit.com.au/txs/pushtx : \n\n`);
     console.log(finalTx.toHex());
@@ -83,24 +79,16 @@ const signTx = function(pubkeys, prvs, path, scripts) {
 const execute = co(function *() {
 
     const p1 = sjcl.decrypt(walletPasscode, JSON.stringify(ep1));
-    const p2 = sjcl.decrypt(walletPasscode, JSON.stringify(ep2));
-    const prvs = [p1, p2];
-
-    if (prvs.length !== 2) {
-        throw new Error('Expected 2 private keys but got ' + prvs.length);
-    }
+    const prvs = [p1];
 
     const node0 = bip32.fromBase58(p1);
-    const node1 = bip32.fromBase58(p2);
-    const node2 = bip32.fromBase58(bitgoPublicKey);
+    const node1 = bip32.fromBase58(bitgoPublicKey);
     node0.network = network;
     node1.network = network;
-    node2.network = network;
     const path = `0/0/${addressChain}/${addressIndex}`;
     const child0 = node0.derivePath(path);
     const child1 = node1.derivePath(path);
-    const child2 = node2.derivePath(path);
-    const pubkeys = [child0.publicKey, child1.publicKey, child2.publicKey];
+    const pubkeys = [child0.publicKey, child1.publicKey];
 
     const p2ms = bitcoin.payments.p2ms({ m: 2, pubkeys, network });
     const p2wsh = bitcoin.payments.p2wsh({ redeem: p2ms, network });
@@ -113,11 +101,7 @@ const execute = co(function *() {
 
     const result = p2sh;
 
-    if(result.address !== address) {
-        console.log(result.address);
-        console.log(address);
-        throw new Error ('Addresses dont match');
-    }
+    //  Had to remove address check, as the derivation of the address containing the stuck USDT no longer works without the backup key
 
     console.log('Got wallet data, now attempting to sign transaction...');
     signTx(pubkeys, prvs, path, scripts);
